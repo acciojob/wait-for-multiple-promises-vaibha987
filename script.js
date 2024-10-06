d0ad73a1795fcf2a0aa9ab864b0b53c46be24d3e
vaibha987 <v2goyal95@gmail.com>
2024-10-06 15:00:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+
+let promise1 = new Promise((resolve) => {
+  setTimeout(resolve, 1000 + Math.random() * 2000);
+});
+
+let promise2 = new Promise((resolve) => {
+  setTimeout(resolve, 1000 + Math.random() * 2000);
+});
+
+let promise3 = new Promise((resolve) => {
+  setTimeout(resolve, 1000 + Math.random() * 2000);
+});
+
+let bigPromise=Promise.all([promise1,promise2,promise3])
+bigPromise.then(() => {
+  console.log('All promises have resolved');
+});
