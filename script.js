233f3f3620b47b31b41c3b1b2cd9723613515490
vaibha987 <v2goyal95@gmail.com>
2024-10-06 15:07:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-//your JS code here. If required.
+let startTime = Date.now();
 
 let promise1 = new Promise((resolve) => {
-  setTimeout(resolve, 1000 + Math.random() * 2000);
+  setTimeout(() => {
+    let timeTaken = Date.now() - startTime;
+    resolve(timeTaken);
+  }, 1000 + Math.random() * 2000);
 });
 
 let promise2 = new Promise((resolve) => {
-  setTimeout(resolve, 1000 + Math.random() * 2000);
+  setTimeout(() => {
+    let timeTaken = Date.now() - startTime;
+    resolve(timeTaken);
+  }, 1000 + Math.random() * 2000);
 });
 
 let promise3 = new Promise((resolve) => {
-  setTimeout(resolve, 1000 + Math.random() * 2000);
+  setTimeout(() => {
+    let timeTaken = Date.now() - startTime;
+    resolve(timeTaken);
+  }, 1000 + Math.random() * 2000);
 });
 
 let bigPromise=Promise.all([promise1,promise2,promise3])
-bigPromise.then(() => {
+
+bigPromise.then((values) => {
   console.log('All promises have resolved');
+  console.log('Time taken by promise1: ', values[0]);
+  console.log('Time taken by promise2: ', values[1]);
+  console.log('Time taken by promise3: ', values[2]);
+  console.log('Total time taken: ', Date.now() - startTime);
 });
